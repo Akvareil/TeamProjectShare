b2b5648ed8e4b5c8702c8fb163bb1d527a576f1d
Akvareil <andrej.shablovskij@gmail.com>
2015-01-30 12:56:02 +0200

    Add project files

---
+++ Worker.java
@@ -0,0 +1,70 @@
+﻿package parserJSON;
+
+import java.util.Comparator;
+
+import org.apache.commons.lang3.builder.CompareToBuilder;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Worker implements Comparable<Worker> {
+	public String lastName;
+	private String post;
+	private Double salary;
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPost() {
+		return this.post;
+	}
+
+	public void setPost(String post) {
+		this.post = post;
+	}
+
+	public Double getSalary() {
+		return this.salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Lastname: " + this.getLastName() + ", Post: " + this.getPost()
+				+ ", Salary: " + this.getSalary();
+	}
+	
+	@Override
+	public int compareTo(Worker arg0) {
+		return 0;
+	}
+	
+	//Sort by Lastname as default. If Lastname=Lastname - for this rows use one more sort by another field - Salary.
+	public static Comparator<Worker> CompareByNameSalary = new Comparator<Worker>() {
+		@Override
+		public int compare(Worker w1, Worker w2) {
+			if (w1.getLastName().equals(w2.getLastName()) == false) {
+				return new CompareToBuilder()
+						.append(w1.getLastName(), w2.getLastName())
+						.append(w1.getPost(), w2.getPost())
+						.append(w1.getSalary(), w2.getSalary())
+						.toComparison();
+			} else {
+				return new CompareToBuilder()
+						.append(w1.getSalary(), w2.getSalary())
+						.append(w1.getPost(), w2.getPost())
+						.append(w1.getLastName(), w2.getLastName())
+						.toComparison();
+			}
+
+		}
+	};
+}
---
+++ WorkerTest.java
@@ -0,0 +1,36 @@
+﻿package parserJSON;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+
+public class WorkerTest {
+
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+
+		InputStream input = new FileInputStream("workers.json");
+		ObjectMapper mapper = new ObjectMapper();
+		//mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
+		TypeFactory typeFactory = TypeFactory.defaultInstance();
+		List<Worker> workersList = mapper.readValue(input,
+				typeFactory.constructCollectionType(ArrayList.class, Worker.class));
+		
+		System.out.println("Sort by LastName + Salary(if LastName=LastName)\n");
+		Collections.sort(workersList, Worker.CompareByNameSalary);
+		for(Worker worker : workersList){
+			System.out.println("Worker"+(workersList.indexOf(worker)+1)+" - "+worker);
+		} 
+		
+	}
+}
